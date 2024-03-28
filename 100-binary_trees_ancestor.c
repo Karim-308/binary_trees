e60b50ae6addc6278b67834328f0ae59cb925261
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-28 04:19:17 +0200

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,14 +8,14 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
+	size_t depth = 0;
 
-    while (tree && tree->parent)
-    {
-        depth++;
-        tree = tree->parent;
-    }
-    return (depth);
+	while (tree && tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
 }
 
 /**
@@ -26,33 +26,33 @@ size_t binary_tree_depth(const binary_tree_t *tree)
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-    size_t depth_first, depth_second;
+	size_t depth_first, depth_second;
+	
+	if (!first || !second)
+		return (NULL);
 
-    if (!first || !second)
-        return (NULL);
+	depth_first = binary_tree_depth(first);
+	depth_second = binary_tree_depth(second);
 
-    depth_first = binary_tree_depth(first);
-    depth_second = binary_tree_depth(second);
+	while (depth_first > depth_second)
+	{
+		first = first->parent;
+		depth_first--;
+	}
 
-    while (depth_first > depth_second)
-    {
-        first = first->parent;
-        depth_first--;
-    }
+	while (depth_second > depth_first)
+	{
+		second = second->parent;
+		depth_second--;
+	}
 
-    while (depth_second > depth_first)
-    {
-        second = second->parent;
-        depth_second--;
-    }
+	while (first && second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+		first = first->parent;
+		second = second->parent;
+	}
 
-    while (first && second)
-    {
-        if (first == second)
-            return ((binary_tree_t *)first);
-        first = first->parent;
-        second = second->parent;
-    }
-
-    return (NULL);
+	return (NULL);
 }

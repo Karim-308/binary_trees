88218baf4f3030fbb1f6637f0c7e2944934b0cf1
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-28 04:31:13 +0200

    Update 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -13,19 +13,19 @@ int binary_tree_is_root(const binary_tree_t *node)
     /* Check if the node is NULL */
 	if (node == NULL)
 	{
-        /* If node is NULL, it cannot be a root */
+	/* If node is NULL, it cannot be a root */
 		return (0);
 	}
 
-    /* Check if the node has a parent */
+	/* Check if the node has a parent */
 	if (node->parent == NULL)
 	{
-        /* If node does not have a parent, it is a root */
+	/* If node does not have a parent, it is a root */
 		return (1);
 	}
 	else
 	{
-        /* If node has a parent, it is not a root */
+	/* If node has a parent, it is not a root */
 		return (0);
 	}
 }

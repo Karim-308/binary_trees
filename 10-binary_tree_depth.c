044d6de089dab61eadc08327506c0e4f033893ed
Karim-308 <2001118@eng.asu.edu.eg>
2024-03-28 02:30:33 +0200

    depth and binary_tree_height

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - gets the depth of a node.
+ *
+ * @tree: A pointer to the node.
+ *
+ * Return: The depth of the node, or 0 if tree is NULL.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    size_t depth = 0;
+
+    /* Traverse up the tree to find the depth */
+    while (tree && tree->parent)
+    {
+        tree = tree->parent;
+        depth++;
+    }
+
+    return (depth);
+}
